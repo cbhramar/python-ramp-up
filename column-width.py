2ebd322fc81383da941e6fbfa7d0e2453f623351
Bhramar Choudhary <bchoudha@jaguarlandrover.com>
2020-11-08 14:19:01 +0530

    Column width problem

---
+++ column-width.py
@@ -0,0 +1,22 @@
+def adjustWidth(string,width):
+	length = len(string)
+
+	if length <= width:
+		print(string)
+		return
+
+	out=""
+	for i in range(0,length):
+		if i==0 or i%width!=0:
+			out+=string[i]
+		else:
+			print(out)
+			out=string[i]
+	print(out)
+
+if __name__ == '__main__':
+	s="Lorem ipsum enim voluptatibus porro veritatis. Hic eligendi nihil ut "
+	s+="consequatur. Ut tenetur praesentium sunt eum alias est deserunt esse."
+	s+=" Nesciunt libero cupiditate"
+	r="asdfg"
+	adjustWidth(s,9)
